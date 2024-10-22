b3725e5be627304feb6b6728d13af1c37347aff2
Aytuğ Atasever <aytuuc1@gmail.com>
2024-10-22 20:19:45 +0300

    Update Program.cs
    Co-authored-by:  ercansahin16  <esahin.ast@gmail.com>

--- CSharpEgitimKampi/02_Variables/Program.cs
+++ CSharpEgitimKampi/02_Variables/Program.cs
@@ -13,7 +13,7 @@ namespace _02_Variables
       static void Main(string[] args)
       {
          Console.OutputEncoding = Encoding.UTF8;
-         #region Double Değişkenler
+         #region Double Değişkenler ve kullanım alanları
 
          Console.WriteLine("******************");
 
